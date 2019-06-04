d4974288e7516355d3b2bb1e0a2de985b489bb9c
Francisco <fhernandez@nearsoft.com>
2019-06-04 17:03:42 -0600

    Enable cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const routes = require('./routes');
 
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(routes());
 
